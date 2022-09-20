cbf38132efa6c46494762de157f24d985676eb2d
Sandyra2 <sandrakosisochukwu183@gmail.com>
2022-09-20 14:18:22 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,32 +6,22 @@
  *
  *  * _puts - Write a function that prints a string, followed
  *
- *   * by a new line, to stdout.
+ *  * by a new line, to stdout.
  *
- *    * @str: an input string
+ *  * @str: an input string
  *
- *     * Return: Nothing
+ *  * Return: Nothing
  *
- *      */
+ */
 
 void _puts(char *str)
-
 {
-
-		int i = 0;
-
-
-
-			while (str[i] != '\0')
-
-					{
-
-								_putchar(str[i]);
-
-										i++;
-
-											}
-
-				_putchar('\n');
-
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++,
+	}
+	_putchar('\n');
+	i
 }
